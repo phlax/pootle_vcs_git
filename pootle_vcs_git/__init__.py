4b214d48da75fa6df243ba67fd70aa5f6a8479af
Ryan Northey <ryan@space23.net>
2015-09-24 15:26:43 +0100

    vcs

--- pootle_vcs_git/__init__.py
+++ pootle_vcs_git/__init__.py
@@ -1,6 +1,13 @@
+from glob import glob
+import io
+from itertools import chain
 import os
+from ConfigParser import ConfigParser
 
 from git import Repo
+
+from pootle_language.models import Language
+from pootle_translationproject.models import TranslationProject
 
 from pootle_vcs import Plugin, plugins
 
@@ -29,13 +36,40 @@ class GitPlugin(Plugin):
     def pull(self):
         if not self.is_cloned:
             Repo.clone_from(self.vcs.url, self.local_repo_path)
+        self.repo.remote().pull()
 
     def get_latest_commit(self):
         self.pull()
         return self.repo.commit().hexsha
 
+    def find_translation_files(self):
+        config = self.read_config()
+        translation_path = os.path.join(self.local_repo_path,
+                                        config.get("pootle", "translation_path"))
+        # todo: add mapping of exts/project_type somewhere
+        return (chain.from_iterable(glob(os.path.join(x[0], '*.po'))
+                                    for x in os.walk(translation_path)))
+
     def pull_translation_files(self):
-        pass
+        for f in self.find_translation_files():
+            path, ext = os.path.splitext(f)
+            lang = Language.objects.get(code=os.path.basename(path))
+            try:
+                tp = self.vcs.project.translationproject_set.get(language=lang)
+            except TranslationProject.DoesNotExist:                
+                import pdb; pdb.set_trace()
+
+    def read(self, path):
+        target = os.path.join(self.local_repo_path, path)
+        with open(target) as f:
+            content = f.read()
+        return content
+
+    def read_config(self):
+        self.pull()
+        config = ConfigParser()
+        config.readfp(io.BytesIO(self.read(self.vcs.pootle_config)))
+        return config
 
 
 plugins.register(GitPlugin)
